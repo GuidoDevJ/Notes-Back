08fc14655c6737b57f5f3b337998e4dc909ea0e0
GuidoDevJ <guidogauna9gmail.com>
2023-05-29 11:53:34 -0300

    publish

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 // Importaciones
-import { Connection } from "./database/mongodb";
-import { app } from "./app";
+import { Connection } from "../database/mongodb";
+import { app } from "../app";
 
 // Estableciendo la coneccion
 Connection();
